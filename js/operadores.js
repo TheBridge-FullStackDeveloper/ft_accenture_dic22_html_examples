26cd1b8b6fc200a7eeef8cc17324a2b19c06b76f
Juan Pablo Fuentes <incidencias@gmail.com>
2023-01-10 19:45:24 +0100

    js

---
+++ js/operadores.js
@@ -0,0 +1,26 @@
+let precio = 100
+
+precio = precio + 10
+
+precio = precio * 1.2
+
+console.log(precio)
+
+precio += 20 //precio=precio+20
+
+precio -= 15 //precio=precio-15
+
+precio *= 100 //precio=precio*100
+
+precio++ //precio=precio+1
+
+precio-- //precio=precio-1
+
+let resto = precio % 3 //Módulo o resto de la división
+
+// 7 dividido entre 3 =2 y de resto 1
+
+let cadena = 'hola'
+let otraCadena = ' que tal'
+
+console.log(cadena + otraCadena)
---
+++ js/script.js
@@ -0,0 +1,58 @@
+'use strict'
+
+//Variables
+
+let edad
+
+console.log(edad)
+
+edad = 70
+
+console.log(edad)
+
+edad = 90
+
+console.log(edad)
+
+edad = edad - 20
+
+console.log(edad)
+
+let joven = edad / 2
+
+console.log(joven)
+
+let nombre = 'Ana'
+
+console.log(nombre)
+
+edad = 'hola que tal'
+nombre = 27
+
+console.log(edad)
+console.log(nombre)
+
+let chequeado = false
+
+let nada = null
+
+let cadena //Indefinida
+
+cadena = null //Tiene el valor nulo que es como decir que su valor es no tener valor
+
+cadena = '' // Tiene de valor la cadena vacía
+
+cadena = 'hola'
+cadena = 'hola'
+cadena = `hola ${edad}`
+console.log(cadena)
+let poblacion = "L'hospitalet"
+
+/*
+Esto son comentarios
+de varias líneas
+*/
+
+console.log(typeof cadena)
+
+let cadena = 5
