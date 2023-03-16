856943b6f8d693d28b99a76f9cadb5e6395880e2
Juan Pablo Fuentes <incidencias@gmail.com>
2023-03-16 21:07:49 +0100

    update/delete

--- TestApi/js/code.js
+++ TestApi/js/code.js
@@ -1,8 +1,12 @@
 // Capturo el elemento botón añadir
 let addButton = document.getElementById('add')
+let updateButton = document.getElementById('update')
+let deleteButton = document.getElementById('delete')
 
 // Le añado un evento
 addButton.addEventListener('click', addAula)
+updateButton.addEventListener('click', updateAula)
+deleteButton.addEventListener('click', deleteAula)
 
 // Tendré que hacer lo necesario para añadir el aula
 function addAula () {
@@ -22,11 +26,39 @@ function addAula () {
   })
 }
 
+function updateAula () {
+  let id = document.getElementById('id').value
+  let nombre = document.getElementById('nombre').value
+  let capacidad = document.getElementById('capacidad').value
+  let datos = { nombre: nombre, capacidad: capacidad }
+  service.update(id, datos).then(result => {
+    if (result.error) {
+      console.log('Error modificando')
+    } else {
+      console.log('aula modificada correctamente')
+    }
+  })
+}
+function updateAula () {
+  console.log('Hay que eliminar el aula')
+  let id = document.getElementById('id').value
+  service.remove(id).then(result => {
+    if (result.error) {
+      console.log('Error eliminando')
+    } else {
+      console.log('aula eliminada correctamente')
+    }
+  })
+}
+
 service.getAll().then(data => {
+  console.log(data.Data)
+  let datos_convertidos = JSON.parse(data.Data)
+  console.log(datos_convertidos)
   // Voy a rellenar la tabla con los datos que me envían
   let tabla = document.getElementById('paises')
   tabla.innerHTML = ''
-  for (let pais of data) {
+  for (let pais of datos_convertidos) {
     // pais.country_id pais.country
     let fila = tabla.insertRow(-1)
     let celda_id = fila.insertCell(-1)
